9534d2f4f329c561470fce070a241d817c8227af
Magnus Markling <magnus@markling.com>
2026-03-19 17:20:45 +0100

    wip

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -16,12 +16,12 @@ apt_get_cmd install -yqq software-properties-common
 
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:deadsnakes/ppa -y
-apt_get_cmd update -yqq
 
 apt_get_cmd install -yqq wget make clang-format gcc lcov git openssl libssl-dev \
     unzip rsync build-essential gcc-11 g++-11 curl libclang-dev gdb
 
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+$MODE update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-11 60
 
 # Need clang for LTO
 source "$(dirname "${BASH_SOURCE[0]}")/install_llvm.sh" $MODE
